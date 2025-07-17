ef8bf3adb90da6e86aebc7ee48624a8905c3d8cb
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-07-17 13:52:43 +0200

    refactor

--- src/GameOfLife.hs
+++ src/GameOfLife.hs
@@ -1,18 +1,21 @@
 module GameOfLife (tick) where
 
 tick :: [[Int]] -> [[Int]]
-tick board = [[applyRules c (liveAdj i j) | (j, c) <- zip [0 ..] row] | (i, row) <- zip [0 ..] board]
+tick [] = []
+tick board = [[applyRules c (liveAdjecents i j) | (j, c) <- zip [0 ..] row] | (i, row) <- zip [0 ..] board]
   where
     rows = length board
     cols = length (head board)
 
-    liveAdj :: Int -> Int -> Int
-    liveAdj i j = length $ filter (== 1) [board !! x !! y | (x, y) <- validAdj i j]
-      where
-        validAdj a b = [(x, y) | dx <- [-1, 0, 1], let x = a + dx, dy <- [-1, 0, 1], let y = b + dy, (x, y) /= (a, b), x >= 0, x < rows, y >= 0, y < cols]
+    liveAdjecents i j =
+      sum
+        [ board !! x !! y
+          | x <- [max 0 (i - 1) .. min (rows - 1) (i + 1)],
+            y <- [max 0 (j - 1) .. min (cols - 1) (j + 1)],
+            (x, y) /= (i, j)
+        ]
 
-    applyRules :: Int -> Int -> Int
     applyRules c n
-      | c == 1 = if n `elem` [2, 3] then 1 else 0
-      | c == 0 = if n == 3 then 1 else 0
+      | c == 1 = fromEnum $ n `elem` [2, 3]
+      | c == 0 = fromEnum $ n == 3
       | otherwise = 0
